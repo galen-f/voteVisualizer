ee09c3cf1a60f66b5f0ebf705937000d30ae173e
Galen Ferrara <galen.ferrara557@gmail.com>
2025-09-09 11:21:38 +0100

    Fixed issue with house votes not joining properly

--- src/geo/join_geo.py
+++ src/geo/join_geo.py
@@ -4,21 +4,25 @@ import geopandas as gpd
 def join_votes(chamber, votes, shapes):
     # Detect if house or senate via the chamber argument
     if chamber == "senate":
+        print("Joining states and senate Data...")
         return join_votes_state(votes, shapes)
     elif chamber == "house":
+        print("Joining districts and house Data...")
         return join_votes_district(votes, shapes)
     else:
         raise ValueError("Invalid chamber")
 
+# Helper function to join votes with shapes for senate (states)
 def join_votes_state(votes, shapes):
     joined = pd.merge(
         left=shapes,
         right=votes,
         left_on='STUSPS',
-        right_on='state',
+        right_on='geoid',
         how='left')
     return joined
 
+# Helper function to join votes with shapes for house (districts)
 def join_votes_district(votes, shapes):
     df_agg = votes.groupby("geoid", as_index=False)["vote"].first()
     merged = shapes.merge(
